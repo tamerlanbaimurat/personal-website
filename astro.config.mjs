2df457b1944bae99aba2009a13c74b80b742f382
Tamerlan Baimurat <tapin130505@gmail.com>
2025-05-21 16:02:30 -0400

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://tamerlanbaimurat.github.io',
+	base: '/personal-website',
+	output: 'static',
+})
