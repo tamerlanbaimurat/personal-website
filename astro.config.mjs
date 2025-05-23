eb7d260dc4069cb41b43d3fb0daffe902d4ebc23
Tamerlan Baimurat <tapin130505@gmail.com>
2025-05-23 15:40:52 -0400

    Fixed small bugs (Testing)
    Final version without bugs
    
    Testing
    
    Finally updated icons to be working

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,14 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
 	site: 'https://tamerlanbaimurat.github.io',
 	base: '/personal-website',
+	trailingSlash: 'never',
 	output: 'static',
+	vite: {
+		build: {
+			assetsInlineLimit: 0,
+		},
+	},
+	build: {
+		assets: '_assets',
+	},
 })
