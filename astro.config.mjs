ff59429b8ac61567398ae357cb609bd819a19224
Tamerlan Baimurat <tapin130505@gmail.com>
2026-07-30 16:49:03 -0400

    Small fixes and added info

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
 	site: 'https://tamerlanbaimurat.com',
 	output: 'static',
+	redirects: {
+		'/writing': '/press',
+	},
 })
